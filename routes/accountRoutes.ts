6f01a041d32d07c965ba0de40d01381ccd87a185
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 16:47:01 -0300

    chore: export router

--- routes/accountRoutes.ts
+++ routes/accountRoutes.ts
@@ -4,7 +4,7 @@ import {
   getAllSharesController,
 } from "../controllers/accountControllers";
 
-const router = express.Router();
+export const router = express.Router();
 
 router.get("/savingsAccount", getSavingsController);
 router.get("/savingsAccount/shares", getAllSharesController);
