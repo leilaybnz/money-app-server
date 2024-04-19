23674e62cd75fa99c08f39bb9227fee75dc4f1fa
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 00:56:45 -0300

    feat: create controller to get specific share

--- controllers/accountControllers.ts
+++ controllers/accountControllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import {
   getSavingsService,
   getAllSharesService,
+  getShareService,
 } from "../services/accountService";
 
 export const getSavingsController = (req: Request, res: Response) => {
@@ -19,6 +20,36 @@ export const getAllSharesController = (req: Request, res: Response) => {
   try {
     const allShares = getAllSharesService();
     res.send({ status: "OK", data: allShares });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
+export const getShareController = (req: Request, res: Response) => {
+  const {
+    params: { shareName },
+  } = req;
+
+  if (!shareName) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Parameter ':shareName' cannot be empty." },
+    });
+  }
+
+  try {
+    const share = getShareService(shareName);
+
+    if (!share) {
+      return res.status(404).send({
+        status: "NOT_FOUND",
+        data: { error: `Share with name ${shareName} was not found.` },
+      });
+    }
+
+    res.send({ status: "OK", data: share });
   } catch (error) {
     res
       .status(error?.status || 500)
