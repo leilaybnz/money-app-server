ed6c98776fce786496a4fe9c4e5a83d76fc27b7e
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 16:45:37 -0300

    chore: import and use router

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { router } from "./routes/accountRoutes";
 
 const app = express();
 const port = 5000;
@@ -6,8 +7,10 @@ const port = 5000;
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to the RESTful API!");
+  res.send("Welcome!");
 });
+
+app.use("/savingsAccount", router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
