1337c84633b94cadd87cb4a8a4b25f9d7b930baa
Leila Ybanez <leilanybanez@gmail.com>
2024-04-21 22:37:50 -0300

    feat: create buyShares method

--- database/Account.ts
+++ database/Account.ts
@@ -1,19 +1,56 @@
-import db from "./db.json";
+import { saveToDatabase, readDB } from "./utils";
+import { QuotationSharesPayload } from "../types";
 
 export const getSavings = () => {
-  return db.savingsAccount.cash;
+  return readDB().savingsAccount.cash;
 };
 
 export const getAllShares = () => {
-  return db.savingsAccount.shares;
+  return readDB().savingsAccount.shares;
 };
 
 export const getShare = (shareName: string) => {
-  const share = db.savingsAccount.shares.find(
+  const share = readDB().savingsAccount.shares.find(
     (share) => share.name === shareName
   );
   if (!shareName) {
     return null;
   }
   return share;
+};
+
+export const buyShares = (shareName: string, amount: number) => {
+  const clonedDB = readDB();
+  const account = clonedDB.savingsAccount;
+  const savings = account.cash[0].amount;
+  const shares = account.shares;
+  const share = shares.find((share) => share.name === shareName);
+
+  if (!share) {
+    throw new Error("Share not found");
+  }
+
+  account.shares.forEach((share) => {
+    if (share.name === shareName) {
+      if (share.availableShares < amount) {
+        throw new Error(
+          "No contamos con las suficientes acciones disponibles."
+        );
+      }
+
+      const totalAmount = share.price * amount;
+
+      if (totalAmount > savings) {
+        throw new Error(
+          "No contas con la cantidad de dinero suficiente para realizar esta operacion"
+        );
+      }
+
+      account.cash[0].amount = savings - totalAmount;
+      share.quantityOwned = share.quantityOwned + amount;
+      share.availableShares = share.availableShares - amount;
+    }
+  });
+
+  saveToDatabase(clonedDB);
 };
