a1c92aeab8b9140a7ee24c1431bb69d4021fc190
Leila Ybanez <leilanybanez@gmail.com>
2024-04-21 22:43:02 -0300

    feat: create sellShares controller

--- controllers/accountControllers.ts
+++ controllers/accountControllers.ts
@@ -4,6 +4,7 @@ import {
   getAllSharesService,
   getShareService,
   buySharesService,
+  sellSharesService,
 } from "../services/accountService";
 
 export const getSavingsController = (req: Request, res: Response) => {
@@ -79,6 +80,49 @@ export const buySharesController = (req: Request, res: Response) => {
 
   try {
     buySharesService(shareName, amount);
+
+    res.send({ status: "OK" });
+  } catch (error: any) {
+    if (
+      typeof error === "object" &&
+      error !== null &&
+      "message" in error &&
+      typeof error.message === "string" &&
+      error.message.includes("Share not found")
+    ) {
+      return res.status(404).send({
+        status: "NOT_FOUND",
+        data: { error: `Share with name ${shareName} was not found.` },
+      });
+    }
+
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
+export const sellSharesController = (req: Request, res: Response) => {
+  const {
+    params: { shareName },
+    body: { amount },
+  } = req;
+
+  if (!shareName) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Parameter ':shareName' cannot be empty." },
+    });
+  }
+  if (!amount) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Property 'amount' cannot be empty." },
+    });
+  }
+
+  try {
+    sellSharesService(shareName, amount);
 
     res.send({ status: "OK" });
   } catch (error: any) {
