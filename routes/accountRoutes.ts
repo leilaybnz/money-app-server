39301a3f4fcb71d82a483d0ad57c0a612a78dd63
Leila Ybanez <leilanybanez@gmail.com>
2024-04-21 22:39:28 -0300

    feat: add buyShares route

--- routes/accountRoutes.ts
+++ routes/accountRoutes.ts
@@ -2,9 +2,13 @@ import express from "express";
 import {
   getSavingsController,
   getAllSharesController,
+  getShareController,
+  buySharesController,
 } from "../controllers/accountControllers";
 
 export const router = express.Router();
 
 router.get("/savingsAccount", getSavingsController);
 router.get("/savingsAccount/shares", getAllSharesController);
+router.get("/savingsAccount/shares/:shareName", getShareController);
+router.post("/savingsAccount/shares/:shareName/buy", buySharesController);
